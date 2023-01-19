80f317d52eb06efa715a47afd2582d4b8d954373
YaroslavKabay <yaroslav.kabay@gmail.com>
2023-01-19 17:56:03 +0200

    nestjs routes without db

---
+++ src/user/dto/user.dto.ts
@@ -0,0 +1,28 @@
+import {IsBoolean, IsEmail, IsNotEmpty, IsNumber, IsOptional, IsString, Length, Min} from "class-validator";
+
+export class UserDto {
+    @IsString()
+    @IsOptional()
+    firstName: string;
+
+    @IsString()
+    @IsNotEmpty()
+    @Length(2)
+    surName: string;
+
+    @IsString()
+    @IsEmail()
+    email: string;
+
+
+    @IsNumber()
+    @Min(16)
+    age: number;
+
+    @IsString()
+    city: string;
+
+    @IsBoolean()
+    isConfirmed: string;
+
+}
